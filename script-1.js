aa89456fb268e981907e7d097f51321046265726
aibek <koshmatov2001@gmail.com>
2021-01-25 22:12:09 +0600

    first comit

---
+++ script-1.js
@@ -0,0 +1,38 @@
+ "use strict";
+// let number = 5;
+
+// console.log(number);
+
+// const obj = {
+//     name: 'Aibek',
+//     age: 29,
+//     ismaried: true
+// };
+// console.log(obj.ismaried);
+
+// let user = 'Aibek';
+// let user2 = 'Shiza';
+// console.log(`hello bitch ${user} awegva  ${user2}`);
+
+
+const numberOffilms = prompt(`how many films did u wotch`);
+ 
+const personMovieDB =  {
+    cout: numberOffilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false 
+
+};
+
+const nameOfFilms = prompt(`the last film that you watch`);
+const rareOfFilms = prompt(`your rare to this film`);
+
+const movies = {
+    nameOfFilms: rareOfFilms
+
+};
+ 
+console.log(movies);
+ 
